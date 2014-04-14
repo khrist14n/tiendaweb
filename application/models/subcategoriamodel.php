9e498abf432cb75f0d9ce054701a8b9dcd50ef1a
khrist14n <khrist14n@gmail.com>
2014-04-14 05:18:00 +0000

    Tienda web

---
+++ application/models/subcategoriamodel.php
@@ -0,0 +1,64 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class subcategoriaModel extends CI_Model {
+	public $table;
+	public function  __construct(){
+		parent::__construct();
+		$this->table="subcategoria";
+	}
+
+	public function getAll(){
+		$data=$this->db->get($this->table);
+		$all=array();
+		if($data->num_rows()>0){
+			foreach($data->result() as $row){
+				$all[]=array(
+					"id"=>$row->id,
+					"categoria"=>$row->categoria,
+					"descripcion"=>$row->descripcion,
+					"deleted"=>$row->deleted,
+					"created"=>$row->created,
+					"modified"=>$row->modified
+				);
+			}
+		}
+		return $all;
+	}
+	public function create($data){
+		$this->db->insert($this->table,$data);
+		return $this->db->insert_id();
+	}
+	public function update($id,$data){
+		$this->db->where("id",$id);
+		$this->db->update($this->table,$data);
+		return $this->db->affected_rows();
+	}
+	public function delete($id){
+		$this->db->where("id",$id);
+		$this->db->delete($this->table);
+		return true;
+	}
+	public function getOne($id){
+		$this->db->where("id",$id);
+		$data=$this->db->get($this->table);
+		$all=array();
+		if($data->num_rows()>0){
+			foreach($data->result() as $row){
+				$all=array(
+					"id"=>$row->id,
+					"categoria"=>$row->categoria,
+					"descripcion"=>$row->descripcion,
+					"deleted"=>$row->deleted,
+					"created"=>$row->created,
+					"modified"=>$row->modified
+				);
+			}
+			return $all;
+		}else{
+			return FALSE;
+		}
+	}
+}
+
+/* End of file subcategoriamodel.php */
+/* Location: ./application/models/subcategoriamodel.php */
